8de1fc60a8cb629918ce5f56b77511258918411e
Felipe Gonzalez Hernandez <gambusio@gmail.com>
2023-01-22 17:29:58 +0000

    added adulto view

--- Modulo1/holamundo/holamundo/views.py
+++ Modulo1/holamundo/holamundo/views.py
@@ -5,3 +5,9 @@ def saludo(request):
 
 def despedida(request):
     return HttpResponse("Adiós mundo cruel")
+
+def adulto(request, edad):
+    if edad >= 18:
+        return HttpResponse("Eres mayor de edad")
+    else:
+        return HttpResponse("No eres mayor de edad")
