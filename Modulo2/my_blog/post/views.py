35f75011ab976fe7ac3b6afee405842b3849c7c6
Felipe Gonzalez Hernandez <gambusio@gmail.com>
2023-02-26 18:30:43 +0000

    añadidas consultas

--- Modulo2/my_blog/post/views.py
+++ Modulo2/my_blog/post/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Author, Entry
 
-# Create your views here.
+def queries(request):
+    # Obtener todos los elementos
+    authors = Author.objects.all()
+
+    # Obtener los datos filtrados por condicion
+    filtered = Author.objects.filter(email='victoria29@example.net')
+
+    # Obtener un único objeto
+    author = Author.objects.get(id=1)
+
+    # Obtener los 10 primeros elementos
+    limits = Author.objects.all()[:10]
+
+    # Obtener 10 elementos saltando los 5 primeros
+    offsets = Author.objects.all()[5:10]
+
+    return render(request, 'post/queries.html', 
+                  {'authors': authors, 'filtered': filtered, 'author': author, 'limits': limits, 'offsets': offsets})
